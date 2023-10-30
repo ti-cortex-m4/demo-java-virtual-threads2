c7982a04c4bdfe8104a0fbd7718feea6254b47c8
Aliaksandr Liakh <aaa@epam.com>
2023-10-30 22:06:57 +0300

    Update

---
+++ src/main/java/ds/ex4/src/main/java/tests/ParallelStreamsTest.java
@@ -0,0 +1,102 @@
+package ds.ex4.src.main.java.tests;
+
+import ds.ex4.src.main.java.transforms.Transform;
+import ds.ex4.src.main.java.utils.BlockingTask;
+import ds.ex4.src.main.java.utils.FileAndNetUtils;
+import ds.ex4.src.main.java.utils.Image;
+import ds.ex4.src.main.java.common.Options;
+
+import java.io.File;
+import java.net.URL;
+import java.util.List;
+import java.util.concurrent.ForkJoinPool;
+import java.util.stream.Stream;
+
+/**
+ * Download, transform, and store {@link Image} objects using the Java
+ * parallel streams framework.
+ */
+public class ParallelStreamsTest {
+    /**
+     * This method uses Java parallel streams with reduce()/concat()
+     * to run the test.
+     */
+    public static void run(String testName) {
+        // Store the list of downloaded/transformed images.
+        List<File> imageFiles = Options.instance()
+            // Get the List of URLs.
+            .getUrlList()
+
+            // Convert List into a parallel stream.
+            .parallelStream()
+
+            // Transform URL to an Image by downloading each Image via
+            // its URL.
+            .map(ParallelStreamsTest::downloadImage)
+
+            // Apply transforms to all images, yielding a stream of
+            // stream of images.
+            .map(ParallelStreamsTest::transformImage)
+
+            // Convert the stream of stream of images into a stream
+            // of images without using flatMap().
+            .reduce(Stream::concat).orElse(Stream.empty())
+
+            // Store the images.
+            .map(ParallelStreamsTest::storeImage)
+
+            // Terminate the stream and collect the results into list
+            // of images.
+            .toList();
+
+        // Print the statistics for this test run.
+        Options.instance().printStats(testName,
+                                      imageFiles.size());
+    }
+
+    /**
+     * Transform a {@link URL} to an {@link Image} by downloading the
+     * contents of the {@code url} via the common fork-join
+     * framework's {@link ForkJoinPool.ManagedBlocker} mechanism.
+     *
+     * @param url The {@link URL} of the image to download
+     * @return An {@link Image} containing the image contents
+     */
+    private static Image downloadImage(URL url) {
+        return BlockingTask
+           .callInManagedBlock(() -> FileAndNetUtils
+                               .downloadImage(url));
+    }
+
+    /**
+     * Store the {@link Image} via the common fork-join
+     * framework's {@link ForkJoinPool.ManagedBlocker} mechanism.
+     *
+     * @param image The {@link Image} to store
+     * @return A {@link File} containing the {@link Image}
+     */
+    private static File storeImage(Image image) {
+        return BlockingTask
+            // Store the image via the common fork-join
+            // framework's ManagedBlocker mechanism.
+            .callInManagedBlock(() ->FileAndNetUtils
+                               .storeImage(image));
+    }
+
+    /**
+     * This method applies a group of {@link Transform} objects to
+     * transform the {@link Image}.
+     *
+     * @param image The {@link Image} to transform
+     * @return A {@link Stream} of transformed {@link Image} objects
+     */
+    private static Stream<Image> transformImage(Image image) {
+        return Options.instance().transforms()
+            // Convert the List of transforms to a parallel stream.
+            .parallelStream()
+
+            // Apply each transform to the original image to produce a
+            // transformed image.
+            .map(transform -> transform.transform(image));
+    }
+}
---
+++ src/main/java/ds/ex4/src/main/java/utils/Image.java
@@ -0,0 +1,150 @@
+package ds.ex4.src.main.java.utils;
+
+import ds.ex4.src.main.java.common.Options;
+import ds.ex4.src.main.java.transforms.Transform;
+import ds.ex4.src.main.java.transforms.Transforms;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.net.URL;
+
+import static ds.ex4.src.main.java.utils.ExceptionUtils.rethrowSupplier;
+
+/**
+ * Stores platform-independent meta-data about an Image and also
+ * provides methods for common image- and file-related tasks, such as
+ * decoding raw byte arrays into an Image and setting/getting image
+ * and file names.
+ */
+public class Image {
+    /**
+     * The source URL from which the result was downloaded.
+     */
+    private final URL mSourceUrl;
+
+    /**
+     * The Bitmap our Image stores.
+     */
+    public BufferedImage mImage;
+
+    /**
+     * Name of the transform to apply to the {@link Image}.
+     */
+    private String mTransformName = "";
+
+    /**
+     * Construct an Image from a byte array of {@code imageData}
+     * downloaded from a URL {@code sourceUrl}.
+     */
+    public Image(URL sourceURL,
+                 byte[] imageData) {
+        // Set the image.
+        mImage = rethrowSupplier(() -> ImageIO
+                                 .read(new ByteArrayInputStream(imageData))).get();
+
+        // Set the URL.
+        mSourceUrl = sourceURL;
+    }
+
+    /**
+     * Constructor initialized by the {@link BufferedImage}
+     */
+    private Image(BufferedImage bufferedImage,
+                  URL sourceURL,
+                  String transformName) {
+        mImage = bufferedImage;
+        mSourceUrl = sourceURL;
+        mTransformName = transformName;
+    }
+
+    /**
+     * Returns the file name from the URL this Image was
+     * constructed from.
+     */
+    private String getFileName() {
+        return FileAndNetUtils.getFileNameForUrl(mSourceUrl,
+                                                 mTransformName);
+    }
+
+    /**
+     * Apply the transform to the {@link Image}.
+     */
+    public Image applyTransform(Transform.Type type) {
+        BufferedImage originalImage = mImage;
+        BufferedImage filteredImage =
+            new BufferedImage
+            (originalImage.getColorModel(),
+             originalImage.copyData(null),
+             originalImage.getColorModel().isAlphaPremultiplied(),
+             null);
+
+        int[] pixels = filteredImage
+            .getRGB(0, 0, filteredImage.getWidth(), filteredImage.getHeight(),
+                    null, 0, filteredImage.getWidth());
+
+        int[] lastProgress = new int[1];
+
+        String transformName;
+        switch (type) {
+            case GRAY_SCALE_TRANSFORM -> {
+                Transforms.grayScale(pixels, filteredImage.getColorModel().hasAlpha());
+                transformName = "grayscale";
+            }
+            case TINT_TRANSFORM -> {
+                Transforms.tint(pixels, filteredImage.getColorModel().hasAlpha(),
+                        0.0f, 0.0f, 0.9f);
+                transformName = "tint";
+            }
+            case SEPIA_TRANSFORM -> {
+                Transforms.sepia(pixels, filteredImage.getColorModel().hasAlpha());
+                transformName = "sepia";
+            }
+            default -> {
+                return this;
+            }
+        }
+
+        filteredImage
+            .setRGB(0, 0, filteredImage.getWidth(), filteredImage.getHeight(),
+                    pixels, 0, filteredImage.getWidth());
+
+        return new Image(filteredImage,
+                         mSourceUrl,
+                         transformName);
+    }
+
+    /**
+     * Store the image on the local file system.
+     *
+     * @return The stored image file.
+     */
+    public File store() {
+        // Get a reference to the file in which the image will be
+        // stored.
+        File imageFile = new File(new File(Options.instance().getDirectoryPath()),
+                                  getFileName());
+        
+        // Store the image using try-with-resources
+        try (FileOutputStream outputFile =
+             new FileOutputStream(imageFile)) {
+            // Write the image to the output file.
+
+            BufferedImage bufferedImage = mImage;
+
+            if (bufferedImage == null)
+                System.out.println("null image");
+            else 
+                ImageIO.write(bufferedImage,
+                              "png",
+                              outputFile);
+
+            return imageFile;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
