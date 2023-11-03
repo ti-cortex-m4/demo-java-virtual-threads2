2094a7b5da73b5004783d1c8ac47e63cb1b34c0c
Aliaksandr Liakh <aaa@epam.com>
2023-11-03 16:45:29 +0300

    Update

---
+++ src/main/java/x4/ExecutorsNewVirtualThreadPerTaskExecutor.java
@@ -0,0 +1,17 @@
+package x4;
+
+import java.util.concurrent.*;
+
+public class ExecutorsNewVirtualThreadPerTaskExecutor {
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executor = Executors.newVirtualThreadPerTaskExecutor();
+
+        executor.submit(() -> {
+            System.out.println("runnable");
+        });
+
+        executor.shutdown();
+        executor.awaitTermination(10, TimeUnit.SECONDS);
+    }
+}
