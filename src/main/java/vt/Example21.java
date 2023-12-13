2ed38bcf3976f3317944adce7809f342a3c2ed0e
Aliaksandr Liakh <aaa@epam.com>
2023-12-13 17:24:11 +0300

    Update

---
+++ src/main/java/vt/Example21.java
@@ -0,0 +1,14 @@
+package vt;
+
+import java.util.concurrent.ExecutionException;
+
+public class Example21 {
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        Thread.Builder builder = Thread.ofVirtual()
+            .name("MyThread");
+        Thread thread = builder.start(() -> System.out.println("Hello"));
+        //assertEquals
+        thread.join();
+    }
+}
--- src/main/java/vt/Example3.java
+++ src/main/java/vt/Example3.java
@@ -8,11 +8,11 @@ import java.util.concurrent.Future;
 public class Example3 {
 
     public static void main(String[] args) throws InterruptedException, ExecutionException {
-        try (ExecutorService myExecutor = Executors.newVirtualThreadPerTaskExecutor()) {
-            Future<?> future = myExecutor.submit(() -> System.out.println("Running thread"));
+        try (ExecutorService executorService = Executors.newVirtualThreadPerTaskExecutor()) {
+            System.out.println("executor service: " + executorService);
+            Future<?> future = executorService.submit(() -> System.out.println("Running thread"));
             future.get();
             System.out.println("Task completed");
-            // ...
         }
     }
 }
