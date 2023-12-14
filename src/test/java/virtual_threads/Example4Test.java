3e54657d2e733c92da408746650b9f33f3c0e0a6
Aliaksandr Liakh <aaa@epam.com>
2023-12-14 12:42:06 +0300

    Update

--- src/test/java/virtual_threads/Example4Test.java
+++ src/test/java/virtual_threads/Example4Test.java
@@ -11,20 +11,18 @@ public class Example4Test {
     @Test
     public void doTest() throws InterruptedException, ExecutionException {
         try (var executor = Executors.newVirtualThreadPerTaskExecutor()) {
+            System.out.println(executor);
             Future<String> future1 = executor.submit(() -> "alpha");
-            Future<String> future2 = executor.submit(() -> "omega");
-
-            System.out.println(future1.get() + future2.get());
+            System.out.println(future1.get());
         }
     }
 
     @Test
     public void doNotTest() throws InterruptedException, ExecutionException {
         try (var executor = Executors.newCachedThreadPool(Thread.ofVirtual().factory())) {
+            System.out.println(executor);
             Future<String> future1 = executor.submit(() -> "alpha");
-            Future<String> future2 = executor.submit(() -> "omega");
-
-            System.out.println(future1.get() + future2.get());
+            System.out.println(future1.get());
         }
     }
 }
