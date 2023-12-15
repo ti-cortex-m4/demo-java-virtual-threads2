6a1d4b01cf625a522c2ddf1c30694722207e637d
Aliaksandr Liakh <aaa@epam.com>
2023-12-15 10:25:51 +0300

    Update

--- src/test/java/virtual_threads/Rule1WriteBlockingSynchronousCodeTest.java
+++ src/test/java/virtual_threads/Rule1WriteBlockingSynchronousCodeTest.java
@@ -2,7 +2,10 @@ package virtual_threads;
 
 import org.junit.jupiter.api.Test;
 
+import java.net.http.HttpResponse;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
@@ -12,25 +15,51 @@ public class Rule1WriteBlockingSynchronousCodeTest {
     @Test
     public void doTest() throws InterruptedException, ExecutionException {
         try {
+            Info info = new Info();
             String page = getBody(info.getUrl(), HttpResponse.BodyHandlers.ofString());
             String imageUrl = info.findImage(page);
             byte[] data = getBody(imageUrl, HttpResponse.BodyHandlers.ofByteArray());
             info.setImageData(data);
             process(info);
-        } catch (Exception ex) {
-            t.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+    }
 
+    private void process(Info info) {
     }
 
     @Test
     public void doNotTest() throws InterruptedException, ExecutionException {
-        CompletableFuture.supplyAsync(info::getUrl, pool)
+        Info info = new Info();
+        Executor executor = Executors.newCachedThreadPool(Thread.ofVirtual().factory());
+        CompletableFuture.supplyAsync(info::getUrl, executor)
             .thenCompose(url -> getBodyAsync(url, HttpResponse.BodyHandlers.ofString()))
             .thenApply(info::findImage)
             .thenCompose(url -> getBodyAsync(url, HttpResponse.BodyHandlers.ofByteArray()))
             .thenApply(info::setImageData)
             .thenAccept(this::process)
             .exceptionally(t -> { t.printStackTrace(); return null; });
+    }
+
+    private String getBody(String url, String response) {
+        return " ";
+    }
+}
+
+class Info {
+
+    private String url;
+
+    public String getUrl() {
+        return url;
+    }
+
+
+    public String findImage(String page) {
+        return " ";
+    }
+
+    public void setImageData(byte[] data) {
     }
 }
