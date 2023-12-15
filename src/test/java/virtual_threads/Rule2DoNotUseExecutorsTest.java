84872a8f7b38db737400a9e78486a115bc3a3e68
Aliaksandr Liakh <aaa@epam.com>
2023-12-15 17:15:18 +0300

    Update

--- src/test/java/virtual_threads/Rule2DoNotUseExecutorsTest.java
+++ src/test/java/virtual_threads/Rule2DoNotUseExecutorsTest.java
@@ -7,7 +7,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
-public class Rule1DoNotUseExecutorsTest {
+public class Rule2DoNotUseExecutorsTest {
 
     @Test
     public void doTest() throws InterruptedException, ExecutionException {
--- src/test/java/virtual_threads/Rule3UseThreadLocalVariablesCarefullyTest.java
+++ src/test/java/virtual_threads/Rule3UseThreadLocalVariablesCarefullyTest.java
@@ -2,7 +2,7 @@ package virtual_threads;
 
 import org.junit.jupiter.api.Test;
 
-public class Rule1UseThreadLocalVariablesCarefullyTest {
+public class Rule3UseThreadLocalVariablesCarefullyTest {
 
     @Test
     public void threadLocalVariablesTest() {
