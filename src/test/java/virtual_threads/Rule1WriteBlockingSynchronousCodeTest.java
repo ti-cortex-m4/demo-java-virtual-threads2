0002cd1888864e6b90e625fd44ee8a22171873a3
Aliaksandr Liakh <aaa@epam.com>
2023-12-15 10:41:43 +0300

    Update

--- src/test/java/virtual_threads/Rule1WriteBlockingSynchronousCodeTest.java
+++ src/test/java/virtual_threads/Rule1WriteBlockingSynchronousCodeTest.java
@@ -3,12 +3,10 @@ package virtual_threads;
 import org.junit.jupiter.api.Test;
 
 import java.net.http.HttpResponse;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 
 public class Rule1WriteBlockingSynchronousCodeTest {
 
@@ -16,7 +14,7 @@ public class Rule1WriteBlockingSynchronousCodeTest {
     public void doTest() throws InterruptedException, ExecutionException {
         try {
             Info info = new Info();
-            String page = getBody(info.getUrl(), HttpResponse.BodyHandlers.ofString());
+            String page = getBody1(info.getUrl(), HttpResponse.BodyHandlers.ofString());
             String imageUrl = info.findImage(page);
             byte[] data = getBody(imageUrl, HttpResponse.BodyHandlers.ofByteArray());
             info.setImageData(data);
@@ -34,16 +32,31 @@ public class Rule1WriteBlockingSynchronousCodeTest {
         Info info = new Info();
         Executor executor = Executors.newCachedThreadPool(Thread.ofVirtual().factory());
         CompletableFuture.supplyAsync(info::getUrl, executor)
-            .thenCompose(url -> getBodyAsync(url, HttpResponse.BodyHandlers.ofString()))
+            .thenCompose(url -> getBodyAsync1(url, HttpResponse.BodyHandlers.ofString()))
             .thenApply(info::findImage)
             .thenCompose(url -> getBodyAsync(url, HttpResponse.BodyHandlers.ofByteArray()))
             .thenApply(info::setImageData)
             .thenAccept(this::process)
-            .exceptionally(t -> { t.printStackTrace(); return null; });
+            .exceptionally(t -> {
+                t.printStackTrace();
+                return null;
+            });
     }
 
-    private String getBody(String url, String response) {
+    private String getBody1(String url, HttpResponse.BodyHandler<String> response) {
         return " ";
+    }
+
+    private byte[] getBody(String url, HttpResponse.BodyHandler<byte[]> response) {
+        return new byte[];
+    }
+
+    private CompletableFuture<String> getBodyAsync1(String url, HttpResponse.BodyHandler<String> response) {
+        return CompletableFuture.supplyAsync(() -> " ");
+    }
+
+    private CompletableFuture<byte[]> getBodyAsync(String url, HttpResponse.BodyHandler<byte[]> response) {
+        return CompletableFuture.supplyAsync(() -> new byte[]);
     }
 }
 
