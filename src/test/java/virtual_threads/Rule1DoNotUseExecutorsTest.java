5467e79b75d0e33807ad1e3ca0df4ef609bb4e4b
Aliaksandr Liakh <aaa@epam.com>
2023-12-15 10:04:49 +0300

    Update

--- src/test/java/virtual_threads/Rule1DoNotUseExecutorsTest.java
+++ src/test/java/virtual_threads/Rule1DoNotUseExecutorsTest.java
@@ -7,12 +7,15 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
-public class Example4Test {
+public class Rule1DoNotUseExecutorsTest {
 
     @Test
     public void doTest() throws InterruptedException, ExecutionException {
         try (ExecutorService executorService = Executors.newVirtualThreadPerTaskExecutor()) {
             System.out.println("Executor service: " + executorService);
+
+            Future<String> future = executorService.submit(() -> "alpha");
+            future.get();
         }
     }
 
@@ -21,6 +24,9 @@ public class Example4Test {
         // don't do that
         try (ExecutorService executorService = Executors.newCachedThreadPool(Thread.ofVirtual().factory())) {
             System.out.println("Executor service: " + executorService);
+
+            Future<String> future = executorService.submit(() -> "omega");
+            future.get();
         }
     }
 }
