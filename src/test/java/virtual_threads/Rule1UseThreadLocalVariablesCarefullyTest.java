3e1e849b0c8be1f4c7aedaae100b0bea577d12d8
Aliaksandr Liakh <aaa@epam.com>
2023-12-15 17:05:52 +0300

    Update

---
+++ src/test/java/virtual_threads/Rule1UseThreadLocalVariablesCarefullyTest.java
@@ -0,0 +1,14 @@
+package virtual_threads;
+
+import org.junit.jupiter.api.Test;
+
+public class Rule1UseThreadLocalVariablesCarefullyTest {
+
+    @Test
+    public void threadLocalVariablesTest() {
+    }
+
+    @Test
+    public void scopedValuesTest() {
+    }
+}
