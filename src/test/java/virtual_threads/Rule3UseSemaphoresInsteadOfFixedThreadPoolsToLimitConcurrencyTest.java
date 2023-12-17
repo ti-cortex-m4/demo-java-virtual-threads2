c8fc28c534907476817e22e995f9e9a52314fb0f
Aliaksandr Liakh <aaa@epam.com>
2023-12-17 15:58:03 +0300

    Update

--- src/test/java/virtual_threads/Rule3UseSemaphoresInsteadOfFixedThreadPoolsToLimitConcurrencyTest.java
+++ src/test/java/virtual_threads/Rule3UseSemaphoresInsteadOfFixedThreadPoolsToLimitConcurrencyTest.java
@@ -1,25 +1,34 @@
 package virtual_threads;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.Semaphore;
+
 public class Rule3UseSemaphoresInsteadOfFixedThreadPoolsToLimitConcurrencyTest {
 
-//    ExecutorService es = Executors.newFixedThreadPool(10);
-//...
-//    Result foo() {
-//        try {
-//            var fut = es.submit(() -> callLimitedService());
-//            return f.get();
-//        } catch (...) { ... }
-//    }
-//
-//    Semaphore sem = new Semaphore(10);
-//...
-//    Result foo() {
-//        sem.acquire();
-//        try {
-//            return callLimitedService();
-//        } finally {
-//            sem.release();
-//        }
-//    }
+    ExecutorService executorService = Executors.newFixedThreadPool(10);
 
+    String foo1() throws ExecutionException, InterruptedException {
+       Future<String> future = executorService.submit(() -> limitedService());
+       return future.get();
+    }
+
+
+    Semaphore semaphore = new Semaphore(10);
+
+    String foo2() throws InterruptedException {
+        semaphore.acquire();
+        try {
+            return limitedService();
+        } finally {
+            semaphore.release();
+        }
+    }
+
+
+    private String limitedService() {
+        return "";
+    }
 }
