727c7a367c0bfa2d6bb2fefae122dfe88a0f9f51
Aliaksandr Liakh <aaa@epam.com>
2023-12-18 22:00:44 +0300

    Update

---
+++ src/test/java/virtual_threads/Example5ThreadFactoryTest.java
@@ -0,0 +1,24 @@
+package virtual_threads;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ThreadFactory;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class Example5ThreadFactoryTest {
+
+    @Test
+    public void virtualThreadFactoryTest() throws InterruptedException, ExecutionException {
+        Thread.Builder builder = Thread.ofVirtual();
+        ThreadFactory threadFactory = builder.factory();
+        Thread thread = threadFactory.newThread(() -> System.out.println("run"));
+
+        assertEquals("VirtualThreads", thread.getThreadGroup().getName());
+        assertTrue(thread.isDaemon());
+        assertEquals(5, thread.getPriority());
+    }
+}
