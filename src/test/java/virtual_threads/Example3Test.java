720bf829c0efbc832cca1930bb050e1788abc964
Aliaksandr Liakh <aaa@epam.com>
2023-12-18 11:39:45 +0300

    Update

--- src/test/java/virtual_threads/Example3Test.java
+++ src/test/java/virtual_threads/Example3Test.java
@@ -5,33 +5,37 @@ import org.junit.jupiter.api.Test;
 import java.util.concurrent.ExecutionException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class Example3Test {
 
     @Test
-    public void builderTest() throws InterruptedException, ExecutionException {
-        Thread.Builder builder = Thread.ofVirtual()
+    public void platformThreadBuilderTest() throws InterruptedException, ExecutionException {
+        Thread.Builder builder = Thread.ofPlatform()
+            .group(Thread.currentThread().getThreadGroup())
+            .daemon(false)
+            .priority(10)
+            .stackSize(1024)
             .name("MyThread")
             .inheritInheritableThreadLocals(false)
             .uncaughtExceptionHandler((t, e) -> System.out.printf("Thread %s failed with exception %s", t, e));
         Thread thread = builder.start(() -> System.out.println("run"));
 
-        assertEquals("VirtualThreads", thread.getThreadGroup().getName());
-        assertTrue(thread.isDaemon());
-        assertEquals(5, thread.getPriority());
+        assertEquals("main", thread.getThreadGroup().getName());
+        assertFalse(thread.isDaemon());
+        assertEquals(10, thread.getPriority());
 
         thread.join();
     }
 
     @Test
-    public void builder2Test() throws InterruptedException, ExecutionException {
+    public void virtualThreadBuilderTest() throws InterruptedException, ExecutionException {
         Thread.Builder builder = Thread.ofVirtual()
             .name("MyThread")
             .inheritInheritableThreadLocals(false)
             .uncaughtExceptionHandler((t, e) -> System.out.printf("Thread %s failed with exception %s", t, e));
-        Thread thread = builder.unstarted(() -> System.out.println("run"));
-        thread.start();
+        Thread thread = builder.start(() -> System.out.println("run"));
 
         assertEquals("VirtualThreads", thread.getThreadGroup().getName());
         assertTrue(thread.isDaemon());
