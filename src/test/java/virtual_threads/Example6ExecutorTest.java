993f4449ecfe766c7ce3bb5b9891880ddc2d20d1
Aliaksandr Liakh <aaa@epam.com>
2023-12-19 11:16:29 +0300

    Update

---
+++ src/test/java/virtual_threads/Example6ExecutorTest.java
@@ -0,0 +1,22 @@
+package virtual_threads;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.ThreadFactory;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Example6ExecutorTest {
+
+    @Test
+    public void virtualThreadFactoryTest() throws InterruptedException, ExecutionException {
+        try (ExecutorService executorService = Executors.newVirtualThreadPerTaskExecutor()) {
+            Future<?> future = executorService.submit(() -> System.out.println("run"));
+            future.get();
+        }
+    }
+}
