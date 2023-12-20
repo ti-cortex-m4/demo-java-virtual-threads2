7717a2cbb8992194d9bed71ed1859d4522b15cea
Aliaksandr Liakh <aaa@epam.com>
2023-12-20 14:44:15 +0300

    Update

--- src/test/java/virtual_threads/Rule4UseThreadLocalVariablesCarefullyTest.java
+++ src/test/java/virtual_threads/Rule4UseThreadLocalVariablesCarefullyTest.java
@@ -2,30 +2,54 @@ package virtual_threads;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class Rule4UseThreadLocalVariablesCarefullyTest {
 
     private static final ThreadLocal<String> CONTEXT = new ThreadLocal<>();
 
     @Test
-    public void threadLocalVariablesTest() {
-        System.out.println(CONTEXT.get());
-        CONTEXT.set("one");
-        System.out.println(CONTEXT.get());
-        CONTEXT.remove();
+    public void threadLocalVariablesTest() throws InterruptedException {
+        assertNull(CONTEXT.get());
 
+        CONTEXT.set("zero");
+        assertEquals("zero", CONTEXT.get()); // unconstrained mutability
+
+        doSomething();
+        assertEquals("one", CONTEXT.get()); // unbounded lifetime
+
+        Thread childThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                assertEquals("one", CONTEXT.get()); // expensive inheritance
+            }
+        });
+        childThread.join();
+
+
+        CONTEXT.remove();
+        assertNull(CONTEXT.get());
         //create
         //modify in method - change
         //delete
         //inherit in child thread
     }
 
-    private static final ScopedValue<String> NAME = ScopedValue.newInstance();
+    private void doSomething() {
+        CONTEXT.set("one");
+        assertEquals("one", CONTEXT.get());
+    }
+
+//    private static final ScopedValue<String> NAME = ScopedValue.newInstance();
 
     @Test
     public void scopedValuesTest() {
-        System.out.println(NAME.get());
-        ScopedValue.where(NAME, "one");
-        System.out.println(NAME.get());
+//        System.out.println(NAME.get());
+//        ScopedValue.where(NAME, "one");
+//        System.out.println(NAME.get());
 
         //create
         //modify in method - do not change
