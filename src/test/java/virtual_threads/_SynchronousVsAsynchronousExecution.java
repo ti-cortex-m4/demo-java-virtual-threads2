620e9f01a2708a845e62cb84076741ed169ff654
Aliaksandr Liakh <aaa@epam.com>
2023-12-20 18:40:07 +0300

    Update

--- src/test/java/virtual_threads/_SynchronousVsAsynchronousExecution.java
+++ src/test/java/virtual_threads/_SynchronousVsAsynchronousExecution.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.time.LocalTime;
 import java.util.Date;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
@@ -19,27 +20,13 @@ public class _SynchronousVsAsynchronousExecution {
 
     protected static final Logger logger = LoggerFactory.getLogger(_SynchronousVsAsynchronousExecution.class);
 
-    protected static void sleep(int seconds) {
-        try {
-            TimeUnit.SECONDS.sleep(seconds);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    protected static <T> T sleepAndGet(int seconds, T message) {
-        logger.info(message + " started");
-        sleep(seconds);
-        logger.info(message + " finished");
-        return message;
-    }
 
     @Test
     public void blockingSynchronousStyleTest() throws ExecutionException, InterruptedException {
         try (ExecutorService executorService = Executors.newVirtualThreadPerTaskExecutor()) {
-            long start = new Date().getTime();
+            long startMillis = System.currentTimeMillis();
 
-            Future<Float> future = executorService.submit(() -> {
+            Future<Float> future = executorService.submit(() -> { // non-blocking
                 logger.info("task started");
 
                 int netAmountInUsd = getPriceInEur() * getExchangeRateEurToUsd(); // blocking
@@ -49,17 +36,17 @@ public class _SynchronousVsAsynchronousExecution {
                 return grossAmountInUsd;
             });
 
-            float grossAmountInUsd = future.get();
+            float grossAmountInUsd = future.get(); // blocking
             assertEquals(300, grossAmountInUsd);
 
-            logger.info("task finished in {}", new Date().getTime() - start);
+            logger.info("finished in {} millis", System.currentTimeMillis() - startMillis); // ~ 10000 millis
         }
     }
 
     @Test
     public void blockingAsynchronousStyleTest() throws InterruptedException, ExecutionException {
         try (ExecutorService executorService = Executors.newVirtualThreadPerTaskExecutor()) {
-            long start = new Date().getTime();
+            long startMillis = System.currentTimeMillis();
 
             Future<Integer> priceInEur = executorService.submit(this::getPriceInEur); // non-blocking
             Future<Integer> exchangeRateEurToUsd = executorService.submit(this::getExchangeRateEurToUsd); // non-blocking
@@ -69,21 +56,19 @@ public class _SynchronousVsAsynchronousExecution {
             float grossAmountInUsd = netAmountInUsd * (1 + tax.get()); // blocking
 
             assertEquals(300, grossAmountInUsd);
-            logger.info("task finished in {}", new Date().getTime() - start);
+            logger.info("finished in {} millis", System.currentTimeMillis() - startMillis); // ~ 8000 millis
         }
     }
 
     @Test
     public void nonBlockingAsynchronousStyleTest() throws InterruptedException, ExecutionException {
-        long start = new Date().getTime();
-        logger.info("task started");
+        long startMillis = System.currentTimeMillis();
 
         CompletableFuture.supplyAsync(this::getPriceInEur) // non-blocking
             .thenCombine(CompletableFuture.supplyAsync(this::getExchangeRateEurToUsd), (price, exchangeRate) -> price * exchangeRate) // non-blocking
             .thenCompose(amount -> CompletableFuture.supplyAsync(() -> amount * (1 + getTax(amount)))) // non-blocking
             .whenComplete((grossAmountInUsd, throwable) -> { // non-blocking
                 if (throwable == null) {
-                    logger.info("task finished in {}", new Date().getTime() - start);
                     assertEquals(300, grossAmountInUsd);
                 } else {
                     fail(throwable);
@@ -91,26 +76,7 @@ public class _SynchronousVsAsynchronousExecution {
             })
             .get(); // blocking
 
-        logger.info("task finished");
-    }
-
-    @Test
-    public void testSynchronousVirtualThreads() throws InterruptedException {
-        logger.info("this task started");
-        Date start = new Date();
-
-        var thread = Thread.ofVirtual().start(() ->
-            {
-                int netAmountInUsd = getPriceInEur() * getExchangeRateEurToUsd(); // blocking
-                float tax = getTax(netAmountInUsd); // blocking
-                float grossAmountInUsd = netAmountInUsd * (1 + tax);
-
-                logger.info("this task finished: {} in {}", grossAmountInUsd, (new Date().getTime() - start.getTime()));
-            }
-        );
-        thread.join();
-
-        logger.info("this task finished: in {}", (new Date().getTime() - start.getTime()));
+        logger.info("finished in {} millis", System.currentTimeMillis() - startMillis); // ~ 8000 millis
     }
 
     private int getPriceInEur() {
@@ -125,7 +91,18 @@ public class _SynchronousVsAsynchronousExecution {
         return sleepAndGet(5, 50) / 100f;
     }
 
-//    protected static <T> T sleepAndGet(T message) {
-//        return sleepAndGet(1, message);
-//    }
+    protected static <T> T sleepAndGet(int seconds, T message) {
+        logger.info(message + " started");
+        sleep(seconds);
+        logger.info(message + " finished");
+        return message;
+    }
+
+    protected static void sleep(int seconds) {
+        try {
+            TimeUnit.SECONDS.sleep(seconds);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
