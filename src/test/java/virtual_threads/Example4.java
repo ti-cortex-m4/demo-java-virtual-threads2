1994d15a07a601001effe194e73459dae33d49ff
Aliaksandr Liakh <aaa@epam.com>
2023-12-27 22:08:40 +0300

    Update

--- src/test/java/virtual_threads/Example4.java
+++ src/test/java/virtual_threads/Example4.java
@@ -2,6 +2,11 @@ package virtual_threads;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -18,7 +23,6 @@ public class Example4 {
 
         Thread thread = builder.start(() -> System.out.println("run"));
         System.out.println(thread.getClass().getName()); // java.lang.VirtualThread
-
         thread.join();
     }
 
@@ -26,10 +30,30 @@ public class Example4 {
     public void test2() throws InterruptedException {
         Thread thread = Thread.startVirtualThread(() -> System.out.println("run"));
         System.out.println(thread.getClass().getName()); // java.lang.VirtualThread
-
         thread.join();
     }
 
+    @Test
+    public void test3() throws InterruptedException {
+        Thread.Builder builder = Thread.ofVirtual();
+
+        ThreadFactory threadFactory = builder.factory();
+        System.out.println(threadFactory.getClass().getName()); // java.lang.ThreadBuilders$VirtualThreadFactory
+
+        Thread thread = threadFactory.newThread(() -> System.out.println("run"));
+        System.out.println(thread.getClass().getName()); // java.lang.VirtualThread
+        thread.join();
+    }
+
+    @Test
+    public void test4() throws InterruptedException, ExecutionException {
+        try (ExecutorService executorService = Executors.newVirtualThreadPerTaskExecutor()) {
+            System.out.println(executorService.getClass().getName()); // java.util.concurrent.ThreadPerTaskExecutor
+
+            Future<?> future = executorService.submit(() -> System.out.println("run"));
+            future.get();
+        }
+    }
     private void sleep(int milliseconds) {
         try {
             TimeUnit.MILLISECONDS.sleep(milliseconds);
