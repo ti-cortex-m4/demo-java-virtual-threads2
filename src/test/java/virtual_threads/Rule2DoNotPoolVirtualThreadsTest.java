0188f8c5986281781f981bc55315daaa90081e37
Aliaksandr Liakh <aaa@epam.com>
2023-12-30 20:50:32 +0300

    Update

--- src/test/java/virtual_threads/Rule2DoNotPoolVirtualThreadsTest.java
+++ src/test/java/virtual_threads/Rule2DoNotPoolVirtualThreadsTest.java
@@ -11,8 +11,8 @@ public class Rule2DoNotPoolVirtualThreadsTest {
 
     @Test
     public void doTest() throws InterruptedException, ExecutionException {
-        try (ExecutorService executorService = Executors.newVirtualThreadPerTaskExecutor()) {
-            System.out.println("Executor service: " + executorService);
+        try (var executorService = Executors.newVirtualThreadPerTaskExecutor()) {
+            System.out.println(executorService); // java.util.concurrent.ThreadPerTaskExecutor@23941fb4
 
             Future<String> future = executorService.submit(() -> "alpha");
             future.get();
@@ -22,8 +22,8 @@ public class Rule2DoNotPoolVirtualThreadsTest {
     @Test
     public void doNotTest() throws InterruptedException, ExecutionException {
         // don't do that
-        try (ExecutorService executorService = Executors.newCachedThreadPool(Thread.ofVirtual().factory())) {
-            System.out.println("Executor service: " + executorService);
+        try (var executorService = Executors.newCachedThreadPool(Thread.ofVirtual().factory())) {
+            System.out.println(executorService); // java.util.concurrent.ThreadPoolExecutor@f68f0dc[Running, pool size = 0, active threads = 0, queued tasks = 0, completed tasks = 0]
 
             Future<String> future = executorService.submit(() -> "omega");
             future.get();
--- src/test/java/virtual_threads/Rule5UseSynchronizedBlocksAndMethodsCarefullyTest.java
+++ src/test/java/virtual_threads/Rule5UseSynchronizedBlocksAndMethodsCarefullyTest.java
@@ -1,10 +1,17 @@
 package virtual_threads;
 
-import org.junit.jupiter.api.Test;
-
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Rule5UseSynchronizedBlocksAndMethodsCarefullyTest {
+
+    private final Object lockObject = new Object();
+
+    public String useSynchronizedBlockForExclusiveAccess() {
+        synchronized (lockObject) {
+            return exclusiveResource();
+        }
+    }
+
 
     private final ReentrantLock reentrantLock = new ReentrantLock();
 
@@ -14,15 +21,6 @@ public class Rule5UseSynchronizedBlocksAndMethodsCarefullyTest {
             return exclusiveResource();
         } finally {
             reentrantLock.unlock();
-        }
-    }
-
-
-    private final Object lockObject = new Object();
-
-    public String useSynchronizedBlockForExclusiveAccess() {
-        synchronized (lockObject) {
-            return exclusiveResource();
         }
     }
 
