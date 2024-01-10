f5e1fc65e447df51fbcd43115e358e658190c129
Aliaksandr Liakh <aaa@epam.com>
2024-01-10 22:54:43 +0300

    Update

--- src/test/java/virtual_threads/AbstractTest.java
+++ src/test/java/virtual_threads/AbstractTest.java
@@ -2,7 +2,6 @@ package virtual_threads;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import virtual_threads.part2.Rule2WriteBlockingSynchronousCodeTest;
 
 import java.util.concurrent.TimeUnit;
 
