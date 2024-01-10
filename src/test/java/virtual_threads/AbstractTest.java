2da5e1785587e1d3c6f5731a577d0855eadd46cf
Aliaksandr Liakh <aaa@epam.com>
2024-01-10 20:50:15 +0300

    Update

--- src/test/java/virtual_threads/AbstractTest.java
+++ src/test/java/virtual_threads/AbstractTest.java
@@ -1,8 +1,14 @@
 package virtual_threads;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import virtual_threads.part2.Rule2WriteBlockingSynchronousCodeTest;
+
 import java.util.concurrent.TimeUnit;
 
 public class AbstractTest {
+
+    protected final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     protected void sleep(int milliseconds) {
         try {
