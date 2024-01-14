be7059c262ce6d7b3d0ba29cdfc183d338c82eb1
Aliaksandr Liakh <aaa@epam.com>
2024-01-14 13:27:25 +0300

    Update

--- src/test/java/virtual_threads/part2/Rule3DoNotPoolVirtualThreadsTest.java
+++ src/test/java/virtual_threads/part2/Rule3DoNotPoolVirtualThreadsTest.java
@@ -16,25 +16,20 @@ public class Rule3DoNotPoolVirtualThreadsTest extends AbstractTest {
             try (var executorService = Executors.newVirtualThreadPerTaskExecutor()) {
                 System.out.println(executorService); // java.util.concurrent.ThreadPerTaskExecutor@23941fb4
 
-                executorService.submit(() -> {
-                    sleep(1000);
-                    System.out.println("alpha");
-                });
+                executorService.submit(() -> { sleep(1000); System.out.println("alpha"); });
             }
         }
     }
 
     @Nested
     public class DoNot {
+
         @Test
         public void doNotTest() {
             try (var executorService = Executors.newCachedThreadPool(Thread.ofVirtual().factory())) {
                 System.out.println(executorService); // java.util.concurrent.ThreadPoolExecutor@f68f0dc[Running, pool size = 0, active threads = 0, queued tasks = 0, completed tasks = 0]
 
-                executorService.submit(() -> {
-                    sleep(1000);
-                    System.out.println("omega");
-                });
+                executorService.submit(() -> { sleep(1000); System.out.println("omega"); });
             }
         }
     }
